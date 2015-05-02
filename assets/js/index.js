2618a74680b12812725b2ca1d8604abc10fc8067
Evan Chen <echen@192.168.1.21>
2015-05-01 18:44:16 -0700

    Got rock/biker detection working

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,23 @@
 /**
  * Created by echen on 4/30/15.
+ * NOTE: Remember the SVG X-Y Plane starts at the TOP-LEFT corner
  */
 
 $(document).ready(function(){
     var svgPlants = $('.svgPlants');
-    var svgsmallRock = $('.svgsmallRock');
-    var svgrockGroup = $('.svgrockGroup');
-    var svgBiker = $('#svgBiker');
+    //var svgBiker = $('#svgBiker');
     var svgGroundDirt = $('#svgGroundDirt');
     var svgGroundRoad = $('#svgGroundRoad');
     var svgGroundFound = $('#svgGroundFound');
     var gameData = {
+        rockCounter: 0,
         plantInterval: 50,
+        rockInterval: 5000,
         svgPlantsOrigAttr: [],
         svgBikeOrigAttr: {
             x: $(svgBiker).attr('x'),
             y: $(svgBiker).attr('y')
-        },
-        svgsmallRockOrigAttr: [],
-        svgrockGroupAttr: []
+        }
     };
 
     for(var a = 0; a < svgPlants.length; a++){
@@ -27,19 +26,8 @@ $(document).ready(function(){
             y: $(svgPlants[a]).attr('y')
         };
     }
-    for(var a = 0; a < svgsmallRock.length; a++){
-        gameData.svgsmallRockOrigAttr[a] = {
-            x: $(svgsmallRock[a]).attr('x'),
-            y: $(svgsmallRock[a]).attr('y')
-        }
-    }
-    for(var a = 0; a < svgrockGroup.length; a++){
-        gameData.svgrockGroupAttr[a] = {
-            x: $(svgrockGroup[a]).attr('x'),
-            y: $(svgrockGroup[a]).attr('y')
-        }
-    }
-    setInterval(function(){
+
+    var plantInterval = setInterval(function(){
         for(var i = 0; i < svgPlants.length; i++) {
             var xCoord = $(svgPlants[i]).attr('x');
             if(xCoord < -300){
@@ -48,61 +36,122 @@ $(document).ready(function(){
                 $(svgPlants[i]).attr('x', xCoord - 10);
             }
         }
-        for(var i = 0; i < svgsmallRock.length; i++) {
-            var xCoords = $(svgsmallRock[i]).attr('x');
-            if(xCoords < -300){
-                $(svgsmallRock[i]).attr('x', parseInt(gameData.svgsmallRockOrigAttr[i].x));
+    }, gameData.plantInterval);
+
+    var rockMakeInterval = setInterval(function(){
+
+        //var svgsmallRockHTML = '<image class="svgsmallRock" xlink:href="assets/img/smallRock.png" width="30px" height="30px" x="1150" y="480"/>';
+        //var svgrockGroupHTML = '<image class="svgrockGroup" xlink:href="assets/img/rockGroup.png" width="45px" height="45px" x="1550" y="470"/>';
+
+        d3.select('#svgContainer').append('image')
+            .attr('class', 'svgsmallRock')
+            .attr('id', 'svgsmallRock'+gameData.rockCounter)
+            .attr('xlink:href', 'assets/img/smallRock.png')
+            .attr('width', '30px')
+            .attr('height', '30px')
+            .attr('x', 1150)
+            .attr('y', 480);
+        d3.select('#svgContainer').append('image')
+            .attr('class', 'svgrockGroup')
+            .attr('id', 'svgrockGroup'+gameData.rockCounter)
+            .attr('xlink:href', 'assets/img/rockGroup.png')
+            .attr('width', '45px')
+            .attr('height', '45px')
+            .attr('x', 1750)
+            .attr('y', 470);
+
+        gameData.rockCounter++;
+    }, gameData.rockInterval);
+
+    var rockMoveInterval = setInterval(function(){
+
+        for(var i = 0; i < $('.svgsmallRock').length; i++){
+            var xsCoord = $($('.svgsmallRock')[i]).attr('x');
+
+            if(xsCoord < -100){
+                var id = "#"+$($('.svgsmallRock')[i]).attr('id');
+                //$('.svgsmallRock').splice(i, 1);
+                d3.select(id).remove()
             } else {
-                $(svgsmallRock[i]).attr('x', xCoords - 10);
+                $($('.svgsmallRock')[i]).attr('x', xsCoord - 10);
+                var bikerX1 = parseInt(d3.select('#svgBiker').attr('x'));
+                var bikerX2 = bikerX1 + 190;
+                var bikerY = parseInt(d3.select('#svgBiker').attr('y')) + 140;
+
+                // Biker is on top of rock here
+                if(parseInt($($('.svgsmallRock')[i]).attr('x')) < bikerX2 &&  parseInt($($('.svgsmallRock')[i]).attr('x'))+35 > bikerX1){
+
+                    if(bikerY <= parseInt($($('.svgsmallRock')[i]).attr('y'))+25 && bikerY > parseInt($($('.svgsmallRock')[i]).attr('y'))){
+                        console.log("HITTING SMALL ROCK!!");
+                    }
+                }
             }
         }
-        for(var i = 0; i < svgrockGroup.length; i++) {
-            var xCoordg = $(svgrockGroup[i]).attr('x');
-            if(xCoordg < -300){
-                $(svgrockGroup[i]).attr('x', parseInt(gameData.svgrockGroupAttr[i].x));
+        for(var i = 0; i < $('.svgrockGroup').length; i++){
+            var xrCoord = $($('.svgrockGroup')[i]).attr('x');
+
+            if(xrCoord < -100){
+                var id = "#"+$($('.svgrockGroup')[i]).attr('id');
+                //$('.svgrockGroup').splice(i, 1);
+                d3.select(id).remove()
             } else {
-                $(svgrockGroup[i]).attr('x', xCoordg - 10);
+                $($('.svgrockGroup')[i]).attr('x', xrCoord - 10);
+                var bikerX1 = parseInt(d3.select('#svgBiker').attr('x'));
+                var bikerX2 = bikerX1 + 190;
+                var bikerY = parseInt(d3.select('#svgBiker').attr('y')) + 140;
+
+
+                // Biker is on top of rock here
+                if(parseInt($($('.svgrockGroup')[i]).attr('x')) < bikerX2 &&  parseInt($($('.svgrockGroup')[i]).attr('x'))+45 > bikerX1){
+
+                    if(bikerY <= parseInt($($('.svgrockGroup')[i]).attr('y'))+35 && bikerY > parseInt($($('.svgrockGroup')[i]).attr('y'))){
+                        console.log("HITTING BIG ROCK!!");
+                    }
+                }
             }
         }
     }, gameData.plantInterval);
 
     $("#jumpButton").on("click", function(){
-        $(svgBiker).attr('y', gameData.svgBikeOrigAttr.y-10);
-        setTimeout(function(){
-            svgBiker.attr("xlink:href", "assets/img/BikingUp.png");
-            $(svgBiker).attr('y', gameData.svgBikeOrigAttr.y-25);
-        },100);
-        setTimeout(function(){
-            $(svgBiker).attr('y', gameData.svgBikeOrigAttr.y-40);
-        },200);
-        setTimeout(function(){
-            $(svgBiker).attr('y', gameData.svgBikeOrigAttr.y-55);
-        },300);
-        setTimeout(function(){
-            $(svgBiker).attr('y', gameData.svgBikeOrigAttr.y-70);
-        },400);
-        setTimeout(function(){
-            $(svgBiker).attr('y', gameData.svgBikeOrigAttr.y-85);
-        },500);
-        setTimeout(function(){
-            $(svgBiker).attr('y', gameData.svgBikeOrigAttr.y-70);
-        },600);
-        setTimeout(function(){
-            $(svgBiker).attr('y', gameData.svgBikeOrigAttr.y-55);
-        },700);
-        setTimeout(function(){
-            $(svgBiker).attr('y', gameData.svgBikeOrigAttr.y-40);
-        },800);
-        setTimeout(function(){
-            $(svgBiker).attr('y', gameData.svgBikeOrigAttr.y-25);
-        },900);
-        setTimeout(function(){
-            $(svgBiker).attr('y', gameData.svgBikeOrigAttr.y-10);
-        },1000);
-        setTimeout(function(){
-            svgBiker.attr("xlink:href", "assets/img/BikingPush.png");
-            $(svgBiker).attr('y', gameData.svgBikeOrigAttr.y);
-        },1100);
-        svgBiker.attr("xlink:href", "assets/img/BikingReg.png");
+
+        if(parseInt(d3.select('#svgBiker').attr('y')) === parseInt(gameData.svgBikeOrigAttr.y)){
+            d3.select('#svgBiker').attr('y', parseInt(d3.select('#svgBiker').attr('y'))-30);
+            setTimeout(function(){
+                d3.select('#svgBiker').attr("xlink:href", "assets/img/BikingUp.png");
+                d3.select('#svgBiker').attr('y', parseInt(d3.select('#svgBiker').attr('y'))-25);
+            },100);
+            setTimeout(function(){
+                d3.select('#svgBiker').attr('y', parseInt(d3.select('#svgBiker').attr('y'))-20);
+            },200);
+            setTimeout(function(){
+                d3.select('#svgBiker').attr('y', parseInt(d3.select('#svgBiker').attr('y'))-15);
+            },300);
+            setTimeout(function(){
+                d3.select('#svgBiker').attr('y', parseInt(d3.select('#svgBiker').attr('y'))-15);
+            },400);
+            setTimeout(function(){
+                d3.select('#svgBiker').attr('y', parseInt(d3.select('#svgBiker').attr('y'))-15);
+            },500);
+            setTimeout(function(){
+                d3.select('#svgBiker').attr('y', parseInt(d3.select('#svgBiker').attr('y'))+15);
+            },600);
+            setTimeout(function(){
+                d3.select('#svgBiker').attr('y', parseInt(d3.select('#svgBiker').attr('y'))+15);
+            },700);
+            setTimeout(function(){
+                d3.select('#svgBiker').attr('y', parseInt(d3.select('#svgBiker').attr('y'))+15);
+            },800);
+            setTimeout(function(){
+                d3.select('#svgBiker').attr('y', parseInt(d3.select('#svgBiker').attr('y'))+20);
+            },900);
+            setTimeout(function(){
+                d3.select('#svgBiker').attr('y', parseInt(d3.select('#svgBiker').attr('y'))+25);
+            },1000);
+            setTimeout(function(){
+                d3.select('#svgBiker').attr("xlink:href", "assets/img/BikingPush.png");
+                d3.select('#svgBiker').attr('y', parseInt(d3.select('#svgBiker').attr('y'))+30);
+            },1100);
+            d3.select('#svgBiker').attr("xlink:href", "assets/img/BikingReg.png");
+        }
     });
 });
